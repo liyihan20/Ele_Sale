2c3c64ec327861a9bc696789c36d5a21f3928153
liyihan20 <952538163@qq.com>
2017-07-03 11:07:27 +0800

    添加Excel导出模块

--- Sale_platform_ele/Controllers/FileController.cs
+++ Sale_platform_ele/Controllers/FileController.cs
@@ -1,23 +1,41 @@
-﻿using Sale_platform_ele.Services;
+﻿using Sale_platform_ele.Models;
+using Sale_platform_ele.Services;
 using Sale_platform_ele.Utils;
 using System;
 using System.Configuration;
 using System.IO;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
 
 namespace Sale_platform_ele.Controllers
 {
     public class FileController : BaseController
     {
-        
+        private const string TAG = "文件模块";
+
+        private void Wlog(string log, string sysNo = "", int unusual = 0)
+        {
+            base.Wlog(TAG, log, sysNo, unusual);
+        }
+
+        /// <summary>
+        /// 文件上传视图
+        /// </summary>
+        /// <param name="sysNo"></param>
+        /// <returns></returns>
         public ActionResult UploadFileView(string sysNo)
         {
             ViewData["sysNo"] = sysNo;
             return View();
         }
 
-        //保存附件
+        /// <summary>
+        /// 文件保存方法
+        /// </summary>
+        /// <param name="postedFile"></param>
+        /// <param name="saveName"></param>
+        /// <returns></returns>
         [NonAction]
         private bool SaveFile(HttpPostedFileBase postedFile, string saveName)
         {
@@ -36,6 +54,12 @@ namespace Sale_platform_ele.Controllers
             return result;
         }
 
+        /// <summary>
+        /// 开始上传文件
+        /// </summary>
+        /// <param name="FileData">上传文件流</param>
+        /// <param name="num">流水号</param>
+        /// <returns></returns>
         [AcceptVerbs(HttpVerbs.Post)]
         public ContentResult BeginUploadFile(HttpPostedFileBase FileData, string num)
         {
@@ -50,12 +74,18 @@ namespace Sale_platform_ele.Controllers
                     SaveFile(FileData, finalName);
                 }
                 catch (Exception ex) {
-                    fileName = ex.ToString();
+                    finalName = ex.ToString();
                 }
             }
+            Wlog("开始上传文件,finalName:" + finalName, num);
             return Content(finalName);
         }
 
+        /// <summary>
+        /// 取得文件的基本信息，文件名、大小和上传时间
+        /// </summary>
+        /// <param name="sysNo">流水号</param>
+        /// <returns></returns>
         public JsonResult GetFileInfo(string sysNo)
         {
             var fileName = sysNo + ".rar";
@@ -79,12 +109,22 @@ namespace Sale_platform_ele.Controllers
             });
         }
 
+        /// <summary>
+        /// 文件下载视图
+        /// </summary>
+        /// <param name="sysNo">流水号</param>
+        /// <returns></returns>
         public ActionResult DownloadFileView(string sysNo)
         {
             ViewData["sysNo"] = sysNo;
             return View();
         }
 
+        /// <summary>
+        /// 开始下载文件
+        /// </summary>
+        /// <param name="sysNo">流水号</param>
+        /// <returns></returns>
         public FileStreamResult BeginDownloadFile(string sysNo)
         {
             string fileName = sysNo + ".rar";
@@ -99,7 +139,36 @@ namespace Sale_platform_ele.Controllers
                     return null;
                 }
             }
+            Wlog("开始下载文件", sysNo);
             return File(new FileStream(absoluFilePath, FileMode.Open), "application/octet-stream", Server.UrlEncode(fileName));
+        }
+
+        /// <summary>
+        /// 营业单据列表页面，导出Excel
+        /// </summary>
+        /// <param name="fc">form表单</param>
+        public void ExportSalerExcel(FormCollection fc)
+        {
+            SalerSearchParamModel pm = new SalerSearchParamModel();
+            SomeUtils.SetFieldValueToModel(fc, pm);
+
+            BillSv bill = (BillSv)new BillUtils().GetBillSvInstance(pm.billType);
+
+            Wlog("营业员导出Excel："+JsonConvert.SerializeObject(pm));
+
+            bill.ExportSalerExcle(pm, currentUser.userId);
+        }
+
+        public void ExportAuditorExcel(string billType, FormCollection fc)
+        {
+            AuditSearchParamModel pm = new AuditSearchParamModel();
+            SomeUtils.SetFieldValueToModel(fc, pm);
+
+            BillSv bill = (BillSv)new BillUtils().GetBillSvInstance(billType);
+
+            Wlog("审核人导出Excel：" + JsonConvert.SerializeObject(pm), billType);
+
+            bill.ExportAuditorExcle(pm, currentUser.userId);
         }
 
     }
