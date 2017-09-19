69879f0cc9d1020368fba1e631a17f0b8c943856
liyihan20 <952538163@qq.com>
2017-09-19 14:50:32 +0800

    修复上一次更新的bug，null的不能使用trim方法

--- Sale_platform_ele/Utils/SomeUtils.cs
+++ Sale_platform_ele/Utils/SomeUtils.cs
@@ -111,7 +111,7 @@ namespace Sale_platform_ele.Utils
         public static void SetFieldValueToModel(FormCollection col, object obj)
         {
             foreach (var p in obj.GetType().GetProperties()) {
-                string val = col.Get(p.Name).Trim();//字段值
+                string val = col.Get(p.Name);//字段值
                 string pType = p.PropertyType.FullName;//数据类型
                 if (string.IsNullOrEmpty(val) || val.Equals("null")) continue;
                 if (pType.Contains("DateTime")) {
@@ -133,7 +133,7 @@ namespace Sale_platform_ele.Utils
                     }
                 }
                 else if (pType.Contains("String")) {
-                    p.SetValue(obj, val, null);
+                    p.SetValue(obj, val.Trim(), null);
                 }
                 else if (pType.Contains("Bool")) {
                     bool bl;
